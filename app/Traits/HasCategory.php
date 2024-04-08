d4ab7ae3579e37aac4361e5ed075c0e7752b2acd
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-04-08 09:44:43 +0700

    update has category

--- app/Traits/HasCategory.php
+++ app/Traits/HasCategory.php
@@ -8,7 +8,7 @@ trait HasCategory {
 
     public function categories()
     {
-        return $this->morphToMany(Category::class, 'entity', 'base_categorizables', null, 'entity_id');
+        return $this->morphToMany(Category::class, 'entity', 'base_categorizables', null, 'category_id');
     }
 
 }
